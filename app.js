3127f20d58cec4b8da1d180e63bb50a7584a7599
CodeExcalibar <Alex.raju1010@gmail.com>
2025-10-27 20:55:06 +0000

    Established a database connection and used .env to secure credentials

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
+const { Sequelize } = require("sequelize");
 
-app.get("/", (req, res) => {
+const sequelize = new Sequelize({
+	dialect: "mysql",
+	database: process.env.DB_NAME,
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+});
+
+const initializeDatabase = async () => {
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+};
+
+initializeDatabase();
+
+app.get("/", (_, res) => {
 	res.send("Hello World!");
 });
 
