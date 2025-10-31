336fc70c86889a12bdab8857e9d5c58fbd633bdc
CodeExcalibar <Alex.raju1010@gmail.com>
2025-10-31 17:46:35 +0000

    Implemented search functionilty

--- public/script.js
+++ public/script.js
@@ -1,9 +1,44 @@
 const notesContainer = document.querySelector("#note-grid");
-const userCardTemplate = document.querySelector("[data-note-card-template]");
+const noteCardTemplate = document.querySelector("[data-note-card-template]");
+const searchInput = document.querySelector("#search-input");
 
-async function loadNotes() {
+let notesList = [];
+
+const createNoteCard = (note) => {
+	const card = noteCardTemplate.content.cloneNode(true).children[0];
+
+	const header = card.querySelector("[data-header]");
+	const content = card.querySelector("[data-content]");
+
+	// Set the content
+	header.textContent = note.title;
+	content.textContent = note.content;
+
+	return card;
+};
+
+const renderNotes = (notes) => {
+	notesContainer.innerHTML = "";
+
+	const newNotesList = notes.map((note) => {
+		const cardElement = createNoteCard(note);
+
+		notesContainer.append(cardElement);
+
+		return {
+			title: note.title,
+			content: note.content,
+			element: cardElement,
+		};
+	});
+
+	return newNotesList;
+};
+
+const fetchNotes = async () => {
+	const url = "http://localhost:8000/api/v1/notes";
 	try {
-		const response = await fetch("http://localhost:8000/api/v1/notes");
+		const response = await fetch(url);
 
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
@@ -11,25 +46,28 @@ async function loadNotes() {
 
 		const result = await response.json();
 		const { data: notes } = result;
-
-		renderNotes(notes);
+		return notes;
 	} catch (error) {
-		console.error("Failed to load notes:", error);
+		console.error(`Failed to fetch notes from ${url}:`, error);
+		return [];
 	}
-}
+};
 
-function renderNotes(notes) {
-	notes.forEach((note) => {
-		const card = userCardTemplate.content.cloneNode(true).children[0];
+const loadApplication = async () => {
+	const notes = await fetchNotes();
+	if (notes && notes.length > 0) {
+		notesList = renderNotes(notes);
+	} else {
+		notesContainer.textContent = "No notes to display.";
+	}
+};
 
-		const header = card.querySelector("[data-header]");
-		const content = card.querySelector("[data-content]");
-
-		header.textContent = note.title;
-		content.textContent = note.content;
-
-		notesContainer.append(card);
+searchInput.addEventListener("input", (e) => {
+	const searchValue = e.target.value.toLowerCase();
+	notesList.forEach((note) => {
+		const isVisible = note.title.toLowerCase().includes(searchValue);
+		note.element.classList.toggle("hide", !isVisible);
 	});
-}
+});
 
-loadNotes();
+loadApplication();
